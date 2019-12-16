adfe4ea783aca44854e1f5737911eb4a16a44a06
Abel Antunes <abel.neto@ccc.ufcg.edu.br>
2019-12-16 15:33:51 -0300

    Primeiro commit

---
+++ src/main/java/com/aan/sgar/SgarApplication.java
@@ -0,0 +1,13 @@
+package com.aan.sgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aan/sgar/SgarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aan.sgar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
